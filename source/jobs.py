acab07da59004fd33f10fb1fc34115b5c4e71130
Kreshel Nguyen <kreshel@utexas.edu>
2018-12-06 07:36:32 -0600

    finally fixed indentations

--- source/jobs.py
+++ source/jobs.py
@@ -16,9 +16,9 @@ Please look at previous iterations of this for original source code.
 #
 REDIS_IP = os.environ.get('REDIS_IP', '172.17.0.1')
 try:
-    REDIS_PORT = int(os.environ.get('REDIS_PORT'))
+	REDIS_PORT = int(os.environ.get('REDIS_PORT'))
 except:
-    REDIS_PORT = 6379
+	REDIS_PORT = 6379
 
 # Redis DBs
 DATA_DB = 0
@@ -32,11 +32,11 @@ q = HotQueue("queue", host=REDIS_IP, port=REDIS_PORT, db=QUEUE_DB)
 
 # creates a unique id for job
 def _create_jid():
-    return str(uuid4())
+	return str(uuid4())
 
 # adds a 'job. in front of jid to use as a key
 def _create_job_key(jid):
-    return 'job.{}'.format(jid)
+	return 'job.{}'.format(jid)
 
 # creates a job dictionary object with metadata
 def _create_job(jid, status, start, end, plot):
@@ -56,9 +56,9 @@ def _create_job(jid, status, start, end, plot):
 # retrieves job using job key
 def _get_job_by_job_key(job_key):
 	jid, status, start, end, plot = rd.hmget(job_key, 'id', 'status', 'start', 'end', 'plot')
-    if jid:
-        return _create_job(jid, status, start, end, plot)
-    return None
+	if jid:
+		return _create_job(jid, status, start, end, plot)
+	return None
 
 # retrieves job using jid
 def get_job_by_jid(jid):
@@ -84,50 +84,50 @@ def add_job(start=1850, end=1979, status='submitted'):
 
 # updates job in redis with new status
 def update_job_status(jid, status):
-    job_dict = get_job_by_id(jid)
-    if job_dict:
-        job_dict['status'] = status
-        rd.hmset(_create_job_key(jid), job_dict)
-    else:
-        raise Exception()
+	job_dict = get_job_by_id(jid)
+	if job_dict:
+		job_dict['status'] = status
+		rd.hmset(_create_job_key(jid), job_dict)
+	else:
+		raise Exception()
 
 # deletes a job off of redis queue
 def delete_by_id(jid):
-    rd.delete(_create_job_key(jid))
+	rd.delete(_create_job_key(jid))
 
 def queue_job(jid):
-    q.put(jid)
+	q.put(jid)
 
 def finalize_job(jid, file_path):
-    """Update the job in the db with status and plot once worker has completed it."""
-    job = get_job_by_id(jid)
-    job['status'] = 'completed'
-    job['plot'] = open(file_path, 'rb').read()
-    rd.hmset(jid, job)
+	"""Update the job in the db with status and plot once worker has completed it."""
+	job = get_job_by_id(jid)
+	job['status'] = 'completed'
+	job['plot'] = open(file_path, 'rb').read()
+	rd.hmset(jid, job)
 
 def get_job_plot(jid):
-    """Returns the plot, as binary data, associated with the job"""
-    job_dict = get_job_by_id(jid)
-    if not job_dict['status'] == 'completed':
-        return True, "job not complete."
-    return False, rd.hmget(jid, 'plot')
+	"""Returns the plot, as binary data, associated with the job"""
+	job_dict = get_job_by_id(jid)
+	if not job_dict['status'] == 'completed':
+		return True, "job not complete."
+	return False, rd.hmget(jid, 'plot')
 
 # Execute worker
 def execute_job(jid):
-    """Execute the job. This is the callable that is queued and worked on asynchronously."""
-    job_dict = get_job_by_id(jid)
-    #points = get_data(job['start'], job['end'])
-    #years = [int(p['year']) for p in points]
-    #population = [p['population'] for p in points]
-    #plt.scatter(years, population)
-    points = get_data(job_dict['start'], job['end'])
-    years = [int(p['Year']) for p in points]
-    rainfall = [p['Annual rainfall at fortaleza'] for p in points]
-    plt.scatter(years,rainfall)
+	"""Execute the job. This is the callable that is queued and worked on asynchronously."""
+	job_dict = get_job_by_id(jid)
+	#points = get_data(job['start'], job['end'])
+	#years = [int(p['year']) for p in points]
+	#population = [p['population'] for p in points]
+	#plt.scatter(years, population)
+	points = get_data(job_dict['start'], job['end'])
+	years = [int(p['Year']) for p in points]
+	rainfall = [p['Annual rainfall at fortaleza'] for p in points]
+	plt.scatter(years,rainfall)
 
-    tmp_file = '/tmp/{}.png'.format(jid)
-    plt.savefig(tmp_file, dpi=150)
-    finalize_job(jid, tmp_file)
+	tmp_file = '/tmp/{}.png'.format(jid)
+	plt.savefig(tmp_file, dpi=150)
+	finalize_job(jid, tmp_file)
 
 
 
--- source/worker.py
+++ source/worker.py
@@ -3,12 +3,12 @@ import jobs
 
 @q.worker
 def execute_job(jid):
-    # update job status
-    update_job_status(jid, 'in_progress')
-    
-    # generate a graph from the data
-    execute(jid)
+	# update job status
+	update_job_status(jid, 'in_progress')
+	
+	# generate a graph from the data
+	execute(jid)
 
-    update_job_status(jid, 'completed')
+	update_job_status(jid, 'completed')
 
 execute_job()
